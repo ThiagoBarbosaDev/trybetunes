9f001d3b6777e0eb0cea461eb0779229021197ed
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-07-01 16:14:34 -0300

    style: linting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Album from "./pages/Album";
-import Favorites from "./pages/Favorites";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import Profile from "./pages/Profile";
-import ProfileEdit from "./pages/ProfileEdit";
-import Search from "./pages/Search";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
